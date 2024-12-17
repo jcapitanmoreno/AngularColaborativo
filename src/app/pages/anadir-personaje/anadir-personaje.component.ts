c676ff47b33a35ab5dce54738b3dcbf064dc60ad
jcapitanmoreno <jcapitanmoreno@gmail.com>
2024-12-17 10:31:46 +0100

    vistas actualizadas para emepezar el crud

---
+++ src/app/pages/anadir-personaje/anadir-personaje.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anadir-personaje',
+  standalone: true,
+  imports: [],
+  templateUrl: './anadir-personaje.component.html',
+  styleUrl: './anadir-personaje.component.css'
+})
+export class AnadirPersonajeComponent {
+
+}
---
+++ src/app/pages/borrar-personaje/borrar-personaje.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-borrar-personaje',
+  standalone: true,
+  imports: [],
+  templateUrl: './borrar-personaje.component.html',
+  styleUrl: './borrar-personaje.component.css'
+})
+export class BorrarPersonajeComponent {
+
+}
