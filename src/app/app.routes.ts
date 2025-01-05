f019fb9f400b0c6f71a165e37e57772047ac21aa
JoseAngel <Jangelgiron@gmail.com>
2025-01-05 21:34:49 +0100

    Actualiza campeones, falta solucionar un pequeño error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AnadirPersonajeComponent } from './pages/anadir-personaje/anadir-personaje.component';
 import { BorrarPersonajeComponent } from './pages/borrar-personaje/borrar-personaje.component';
+import { ChampionEditComponent } from './champion-edit/champion-edit.component';
 
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'hola-mundo', component: AnadirPersonajeComponent },
-    { path: 'borrar-personaje', component: BorrarPersonajeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'hola-mundo', component: AnadirPersonajeComponent },
+  { path: 'borrar-personaje', component: BorrarPersonajeComponent },
+  { path: 'edit-champion', component: ChampionEditComponent },
 ];
